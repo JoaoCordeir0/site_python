35c0e9f7cf3be026621632bf9d4c4685a2f81c7d
João Victor Cordeiro <58659032+JoaoCordeir0@users.noreply.github.com>
2022-04-18 12:22:11 -0300

    up

--- main.py
+++ main.py
@@ -6,9 +6,6 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-# route -> hashtagtreinamentos.com/
-# função -> o que você quer exibir naquela página
-# template
 
 @app.route("/")
 def homepage():
@@ -22,9 +19,6 @@ def contatos():
 def usuarios(nome):
     return render_template("usuarios.html", nome = nome)
 
-# colocar o site no ar
 if __name__ == "__main__":
     app.run(debug=True)
-
-    # servidor do heroku
 
