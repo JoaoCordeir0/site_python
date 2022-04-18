c62c62c962e014057af6b7b1a0e4a35777ea4a58
Joao.Cordeiro <joaocordeiro2134@gmail.com>
2022-04-18 12:20:54 -0300

    Site com python

---
+++ main.py
@@ -0,0 +1,30 @@
+# passos para publicar no servidor do heroku
+# pip install Flask
+# pip install gunicorn
+# pip freeze > requirements.txt
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+# route -> hashtagtreinamentos.com/
+# função -> o que você quer exibir naquela página
+# template
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+
+@app.route("/usuarios/<nome>")
+def usuarios(nome):
+    return render_template("usuarios.html", nome = nome)
+
+# colocar o site no ar
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    # servidor do heroku
+
